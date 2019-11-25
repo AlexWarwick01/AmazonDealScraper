62d0b70dbbcc7af6cead7cbdc0063b5585ab6792
Alex Warwick <alexwarwick01@gmail.com>
2019-11-25 10:56:37 +0000

    Changes Total price to ammount saved

--- src/main/java/jsoup.java
+++ src/main/java/jsoup.java
@@ -5,11 +5,11 @@ import org.jsoup.select.Elements;
 
 public class jsoup {
     public static void main(String[] args) throws IOException {
+        //Very basic test - returns the price of some headphones - 119.90
         String url = "https://www.amazon.co.uk/dp/B07BPFW2QD/?cstrackid=16d9d55e-b2e7-4025-893e-75c461e979d7&tag=audiotcsuk-21";
         Document document = Jsoup.connect(url).userAgent("Mozilla/49.0").get();
-        Elements question = document.select("#priceblock_ourprice");
-        System.out.println("Price is " + question.html());
+        Elements question = document.select(".priceBlockSavingsString");
+        System.out.println("You save " + question.html());
+
     }
-
-
 }
